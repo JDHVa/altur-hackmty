1f3242b3148759171208e78e77b6097835ea18ff
Alonso Banda <113001530+Qtrocious@users.noreply.github.com>
2026-09-12 17:45:55 -0600

    C: fix sleep y bloques de color en test_tft

--- hardware/display/test_tft.py
+++ hardware/display/test_tft.py
@@ -1,3 +1,4 @@
+import time
 from luma.core.interface.serial import spi
 from luma.lcd.device import ili9341
 from PIL import Image, ImageDraw
@@ -7,9 +8,13 @@ tft = ili9341(serial, width=320, height=240, rotate=0)
 
 img = Image.new("RGB", (320, 240), color=(10, 25, 47))
 draw = ImageDraw.Draw(img)
-draw.rectangle([(10, 10), (310, 230)], outline=(0, 220, 130), width=3)
-draw.text((60, 70), "CENTINELA ALTUR", fill=(255, 255, 255))
-draw.text((80, 110), "TEST DE PANTALLA", fill=(0, 220, 130))
-draw.text((95, 150), "RPi 5 ONLINE", fill=(100, 180, 255))
+draw.rectangle([(5, 5), (315, 235)], outline=(0, 255, 128), width=4)
+draw.rectangle([(20, 20), (300, 70)], fill=(0, 100, 255))
+draw.rectangle([(20, 80), (300, 130)], fill=(0, 200, 100))
+draw.rectangle([(20, 140), (300, 190)], fill=(255, 50, 50))
+draw.text((30, 35), "CENTINELA ALTUR", fill=(255, 255, 255))
+draw.text((30, 95), "HARDWARE ACTIVO", fill=(0, 0, 0))
+draw.text((30, 155), "ESTADO: OK", fill=(255, 255, 255))
 
 tft.display(img)
+time.sleep(10)
